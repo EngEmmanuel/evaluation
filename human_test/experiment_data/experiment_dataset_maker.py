fe5528d75d06527c34e35bd86d26b31a9b710251
EngEmmanuel <emmanueloladokun@hotmail.co.uk>
2023-01-24 18:31:30 +0000

    Wrote a class with methods that can create single and paired datasets for testing the believability of synthetic images

--- human_test/experiment_data/experiment_dataset_maker.py
+++ human_test/experiment_data/experiment_dataset_maker.py
@@ -1,35 +1,150 @@
+import json
+from os import mkdir
+from shutil import move, copy2
 from pathlib import Path
-from random import seed, sample
+from collections import OrderedDict
+from itertools import tee
+from random import seed, sample, shuffle
 
-seed = 1
+seed(1)
 
 
 class ExperimentDatasetMaker():
 
     def __init__(self, real_data_path, fake_data_paths, set_name, n=10) -> None:
-        self.real_data_path = real_data_path
-        self.fake_data_paths = fake_data_paths
-        self.set_name = set_name
+        '''
+        :n: the number of files to be sampled from each synthetic data dir
+        '''
         self.n = n
+        self.set_name = set_name
+        self.real_data_path, self.fake_data_paths = real_data_path, fake_data_paths
+        self.r_image_paths, self.f_image_paths = [], []
 
-    def _sample_images(path: Path, n:int, ext="png"):
+        self.IMAGE_EXT = ["png", "jpg", "jpeg"]
+
+    @staticmethod
+    def _sample_images(path: Path, n: int, exts=["jpg"]):
         '''
-        Samples :n: files with extension :ext: at the location :path:
+        Samples :n: image files at the location :path:
         '''
-        img_fpaths = [f for f in path.glob(f"*.{ext}")]
+        img_fpaths = [x for ext in exts for x in path.glob(f"*.{ext}")]
         return sample(img_fpaths, n)
 
-    def get_rf_images(self):
+    def _make_initial_dataset_dict(self):
+        '''
+        Creates the skeleton dictionary that will be used to store the orginal filenames
+        and there new ids once anonymised alongside other information
+        '''
+        assert len(self.r_image_paths) == len(self.f_image_paths)
+        self.dataset_dict = OrderedDict()
+
+        for (r_p, f_p) in zip(self.r_image_paths, self.f_image_paths):
+            r_key = '/'.join(r_p.parts[-2:])
+            f_key = '/'.join(f_p.parts[-2:])
+
+            self.dataset_dict[r_key] = {
+                'state': 'r',
+                'src_path': str(r_p),
+            }
+            self.dataset_dict[f_key] = {
+                'state': 'f',
+                'src_path': str(f_p),
+            }
+
+        return
+
+    def _dict_to_json(self, save_path):
+        '''
+        Save dictionary as JSON file
+        '''
+        with open(save_path, "w") as outfile:
+            json.dump(self.dataset_dict, outfile)
+
+    def _copy_and_rename(old_path, new_path):
+        copy2(old_path, new_path)
+
+    def _get_rf_image_paths(self):
+        '''
+        Function that samples image paths from the real and fake image directories
+        '''
         n_real = len(self.fake_data_paths)*self.n
-        r_images = self._sample_images(self.real_data_path, n=n_real)
+        self.r_image_paths = __class__._sample_images(
+            path=self.real_data_path, n=n_real, exts=self.IMAGE_EXT)
 
         for p in self.fake_data_paths:
-            pass
+            self.f_image_paths += self._sample_images(
+                p, self.n, exts=self.IMAGE_EXT)
+        # Shuffle f paths as currently in order of directory it was acquired from
+        self.f_image_paths = sample(
+            self.f_image_paths, len(self.f_image_paths))
 
-    
+        assert len(self.r_image_paths) == len(self.f_image_paths)
 
+        # self.r_image_paths = [Path(f"r_img_{i}") for i in range(20)]
+        # self.f_image_paths = [Path(f"f_img_{i}") for i in range(20)]
 
+    def create_single_experiment_set(self, dest_dir: Path, ext="png"):
+        self._get_rf_image_paths()
+        self._make_initial_dataset_dict()
 
+        mkdir(dest_dir / self.set_name)
+        mkdir(dest_dir / self.set_name / "metadata")
+
+        items = list(self.dataset_dict.items())
+        shuffle(items)
+        for i, (k, v) in enumerate(items):
+            a = 5
+            new_img_name = f"img_{i}.jpg"
+            new_path = dest_dir / self.set_name / new_img_name
+
+            __class__._copy_and_rename(
+                old_path=v["src_path"], new_path=new_path)
+
+            self.dataset_dict[k]["set_path"] = str(new_path)
+            self.dataset_dict[k]["new_name"] = new_img_name
+
+        # Add metadata key to the info file
+        self.dataset_dict["metadata"] = {
+            "set_id": self.set_name,
+            "n_images": i + 1
+        }
+        self._dict_to_json(save_path=dest_dir/self.set_name /
+                           "metadata"/f"{self.set_name}.json")
+
+    def create_paired_experiment_set(self, dest_dir: Path, ext="jpg"):
+        '''
+        Creates the dataset required for comparing real and fake images in pairs
+        '''
+
+        # pairwise() from Itertools Recipes
+        def _pairwise(iterable):
+            "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+            a, b = tee(iterable)
+            next(b, None)
+            return zip(a, b)
+
+        l_or_r = [sample(['r', 'f'], 2)
+                  for _ in range(self.n*len(self.fake_data_paths))]
+
+        mkdir(dest_dir / self.set_name)
+
+        for i, (k_r, v_r), (k_f, v_f) in enumerate(self._pairwise(self.dataset_dict.items())):
+            '''
+            Development paused. One image at a time is currently in favour.
+            #TODO make it such that it skips every other step so that you are effectively
+            looping in chunks of two
+            '''
+            _l = l_or_r[i][0]  # Decides if the real or fake image will be on the left or right
+            if _l == 'f':
+                r_name, f_name = f"pair_{i}{1}.{ext}", f"pair_{i}{0}.{ext}"
+            else:
+                r_name, f_name = f"pair_{i}{0}.{ext}", f"pair_{i}{1}.{ext}"
+
+            r_new_path = dest_dir / self.set_name / r_name
+            f_new_path = dest_dir / self.set_name / f_name
+
+            self._copy_and_rename(v_r["src_path"], r_new_path)
+            self._copy_and_rename(v_f["src_path"], f_new_path)
 
     '''
     true_paths =
@@ -61,11 +176,15 @@ class ExperimentDatasetMaker():
     ]
 
     {
-        "img_001": {
+        "s1/img_001": {
             "test_set_id": "blah",
-            "state": (r or f),
+            "state": f,
             "set_name": "pair_41"
-        }
+        },
+        "real/img_001": {
+            "test_set_id": "blah",
+            "state": r,
+            "set_name": "pair_30"
+        },
     }
     '''
-
